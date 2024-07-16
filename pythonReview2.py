60ce2ebbb8c4933f76166a36790a162895b815b7
nadana24-meet <nadanamouz@gmail.com>
2024-07-16 11:33:56 +0300

    PythonReview

---
+++ pythonReview2.py
@@ -0,0 +1,27 @@
+#1
+def create_youtube_video(title,description):
+	details = {"Title":title,"Description":description,"likes":"0","dislikes":"0",comments:{}}
+	return details
+
+#3
+def like(youtubeVideo):
+	if likes in youtube_video:
+		youtube_video["likes"] +=1
+	return details
+
+#4
+def dislike(youtubeVideo):
+	if likes in youtube_video:
+		youtube_video["dislikes"] +=1
+	return details
+
+#5 
+def add_comment(youtubeVideo,username,comment_text):
+	details[comments][username] = comment_text 
+	return details
+
+#6
+create_youtube_video(video,x)
+like(video)
+dislike(video)
+add_comment(video,nada,hello)
